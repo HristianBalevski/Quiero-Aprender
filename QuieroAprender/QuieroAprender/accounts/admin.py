dcf18f3bb929d3f6e60ac1492d1d4b5cf384b58b
Hristian Balevski <hb.hris.bal@gmail.com>
2024-11-27 00:30:36 +0000

    feat: Made  most of the css responsive

--- QuieroAprender/QuieroAprender/accounts/admin.py
+++ QuieroAprender/QuieroAprender/accounts/admin.py
@@ -2,29 +2,17 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-
     list_display = ['username', 'email', 'first_name', 'last_name', 'birth_date', 'is_staff', 'is_active']
-
 
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('bio', 'profile_photo', 'birth_date')}),
     )
 
-
     add_fieldsets = UserAdmin.add_fieldsets + (
         (None, {'fields': ('bio', 'profile_photo', 'birth_date')}),
     )
 
-
-    add_form = UserAdmin.add_form
-    form = UserAdmin.form
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
+    search_fields = ['username', 'email', 'first_name', 'last_name']
 
